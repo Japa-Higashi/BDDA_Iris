7bf3e7536c62602ff64318ca409b05c3660765d1
Japa-Higashi <62806853+Japa-Higashi@users.noreply.github.com>
2022-05-06 20:58:52 -0300

    Update app.py

--- Iris/app.py
+++ Iris/app.py
@@ -1,8 +1,14 @@
 
 import streamlit as st
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 
 st.write("# Classificação da Íris")
 st.write("## Exemplo de complemento de pétala e sépala")
 st.sidebar.write("### Parametros")
 st.sidebar.slider("Comprimento da sépala", 4.8, 8.0, 5.8, 0.1)
 st.sidebar.slider("Com primento da pétala", 0.9, 7.0, 3.8, 0.1)
+
+with open("objetos.pkl", "rb") as arquivo:
+  ss, dtc = pickle.load(arquivo)
