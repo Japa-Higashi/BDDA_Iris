41ab5465bbc1dbe2b23949a4d274819ac4cacbfe
Japa-Higashi <62806853+Japa-Higashi@users.noreply.github.com>
2022-05-06 20:55:20 -0300

    Update app.py

--- Iris/app.py
+++ Iris/app.py
@@ -3,3 +3,6 @@ import streamlit as st
 
 st.write("# Classificação da Íris")
 st.write("## Exemplo de complemento de pétala e sépala")
+st.sidebar.write("### Parametros")
+st.sidebar.slider("Comprimento da sépala", 4.8, 8.0, 5.8, 0.1)
+st.sidebar.slider("Com primento da pétala", 0.9, 7.0, 3.8, 0.1)
