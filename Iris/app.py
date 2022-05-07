1aa871c5f1bf3c80f2fc2a0ad9dd9cc8235b5107
Japa-Higashi <62806853+Japa-Higashi@users.noreply.github.com>
2022-05-06 21:54:27 -0300

    Update app.py

--- Iris/app.py
+++ Iris/app.py
@@ -8,7 +8,7 @@ st.write("# Classificação da Íris")
 st.write("## Exemplo de complemento de pétala e sépala")
 st.sidebar.write("### Parametros")
 comp_sepala = st.sidebar.slider("Comprimento da sépala", 4.8, 8.0, 5.8, 0.1)
-comp_petala = st.sidebar.slider("Com primento da pétala", 0.9, 7.0, 3.8, 0.1)
+comp_petala = st.sidebar.slider("Comprimento da pétala", 0.9, 7.0, 3.8, 0.1)
 
 with open("Iris/objetos.pkl", "rb") as arquivo:
   ss, dtc = pickle.load(arquivo)
