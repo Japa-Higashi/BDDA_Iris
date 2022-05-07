3c3e0e3cf74a027da1de51249eeb3e5856ba0481
Japa-Higashi <62806853+Japa-Higashi@users.noreply.github.com>
2022-05-06 22:12:41 -0300

    Update apps.py

--- Iris/apps.py
+++ Iris/apps.py
@@ -9,7 +9,7 @@ st.write("## Exemplo de complemento de pétala e sépala")
 st.sidebar.write("### Parametros")
 comp_sepala = st.sidebar.slider("Comprimento da sépala", 4.8, 8.0, 5.8, 0.1)
 comp_petala = st.sidebar.slider("Comprimento da pétala", 0.9, 7.0, 3.8, 0.1)
-larg_sepala = st.sidebar.slider("Largura da pétala", 0.9, 7.0, 3.8, 0.1)
+larg_sepala = st.sidebar.slider("Largura da sépala", 0.9, 7.0, 3.8, 0.1)
 larg_petala = st.sidebar.slider("Largura da pétala", 0.9, 7.0, 3.8, 0.1)
 
 with open("Iris/objeto.pkl", "rb") as arquivo:
