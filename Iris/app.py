6e54e2460c15e239539e22078270a2b63d46ed0f
Japa-Higashi <62806853+Japa-Higashi@users.noreply.github.com>
2022-05-06 21:29:04 -0300

    Update app.py

--- Iris/app.py
+++ Iris/app.py
@@ -10,7 +10,7 @@ st.sidebar.write("### Parametros")
 comp_sepala = st.sidebar.slider("Comprimento da sépala", 4.8, 8.0, 5.8, 0.1)
 comp_petala = st.sidebar.slider("Com primento da pétala", 0.9, 7.0, 3.8, 0.1)
 
-with open("objetos.pkl", "rb") as arquivo:
+with open("Iris/objetos.pkl", "rb") as arquivo:
   ss, dtc = pickle.load(arquivo)
 
   
